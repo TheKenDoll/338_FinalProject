297e0b75dde987f3bb781890e3ece444cf253239
1saacH <isaacmhus@gmail.com>
2023-04-10 00:07:19 -0600

    changed packages

--- setup.py
+++ setup.py
@@ -5,5 +5,5 @@ setup(name ='mylibKIv1',
     description='final project for ensf 338',
     author='Isaac Hus and Kenzie Fjestad',
     author_email='isaac.hus@ucalgary.ca, kenzie.fjestad@ucalgary.ca',
-    packages=['mylibKIv1','mylibKIv1.datastructures']
+    packages=['mylibKIv1','mylibKIv1.datastructures', 'mylibKIv1.datastructures.trees', 'mylibKIv1.datastructures.linear', 'mylibKIv1.datastructures.nodes']
     )
